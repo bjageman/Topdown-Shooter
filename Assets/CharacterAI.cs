f4d95ea6dd566e2b370057fbf2b64f5efa8bafe6
Braden Jageman <bjageman@surgeforward.com>
2018-07-04 13:36:20 -0700

    fixed tile size and added NPC

---
+++ Assets/CharacterAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAI : MonoBehaviour {
+	[SerializeField] Player player;
+
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var direction = new Vector2(
+            player.transform.position.x - transform.position.x,
+            player.transform.position.y - transform.position.y
+        );
+
+        transform.right = direction;
+	}
+}
